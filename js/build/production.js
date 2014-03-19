27ab58dee1e747376a23b2449287857ffafcf63d
Paul Haddad <paulh16@gmail.com>
2014-03-19 09:08:38 +0000

    Dynamically add class to last blog post fon IE8 for bottom borders

--- js/build/production.js
+++ js/build/production.js
@@ -1,4 +1,6 @@
+// Add bottom border on last post for IE8
 
+$('.lt-ie9 .blog-posts .post:last-of-type').addClass('ie-border');
 
 // Avoid `console` errors in browsers that lack a console.
 (function() {
